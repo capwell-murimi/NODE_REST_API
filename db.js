0af897488601cceab38266cde731ef911ef0fcf3
Capwell Murimi <85755317+capwell-murimi@users.noreply.github.com>
2025-03-25 00:10:23 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "students",
-    password: "murimi",
+    user: "students_u0x8_user",
+    host: "cvgl0oqqgecs739gf430-a.oregon-postgres.render.com",
+    database: "students_u0x8",
+    password: "aIngur7lkKiF4nQX9Ov589Ak1Xlyxt3W",
     port: 5432,
 });
+
 
 module.exports = pool;
